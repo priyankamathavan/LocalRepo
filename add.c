2738259f9c69e9c7016fe06f8448df5a8bfca032
priyanka <priyankamathavan@gmail.com>
2016-06-14 17:33:08 +0530

    changing file contents

--- add.c
+++ add.c
@@ -6,8 +6,8 @@ int a = 20;
 int main () {
 
   /* local variable declaration in main function */
-  int a = 4010;
-  int b = 4020;
+  int a = 40;
+  int b = 40;
   int c = 0;
 
   printf ("value of a in main() = %d\n",  a);
